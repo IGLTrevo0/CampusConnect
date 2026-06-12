688339e45fd0689631d3fee96c64af29bc770adc
Aditya Pratap Singh Jadon <adityapratap2904@gmail.com>
2026-06-13 02:49:07 +0530

    removed duplicates root frontend files

--- src/CSEDSocial.jsx
+++
@@ -1,8 +0,0 @@
-function CSEDSocial({ icon, url, name }) {
-  return (
-    <a className="social-link" href={url} target="_blank" rel="noopener noreferrer">
-      <img src={icon} alt={name} />
-    </a>
-  );
-}
-export default CSEDSocial;
