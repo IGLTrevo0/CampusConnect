6e5b674d7c08634d745aced6c79d8685a1cf2c5f
Sithi <sithivignesh11@gmail.com>
2026-06-12 22:37:49 +0530

    feat: auth middleware and /me route

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -43,3 +43,13 @@ exports.login = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+// me
+exports.getMe = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
